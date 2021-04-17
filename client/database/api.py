96d180b8e06eddc52727e94d5b35aace8611732a
Elizabeth Sarell <sc18ess@leeds.ac.uk>
2021-04-17 15:20:48 +0100

    hello im committing

---
+++ client/database/api.py
@@ -0,0 +1,27 @@
+import services
+import users
+import locations
+import bookings
+
+# users functions
+add_user = users.add_user    
+update_membership = users.update_membership
+update_location = users.update_location
+get_user_details = users.get_user_details
+
+#services functions
+add_services = services.add_services
+get_service_id_from_name = services.get_service_id_from_name
+get_service = services.get_service
+update_availabilty = services.update_availabilty
+select_availabilty = services.select_availabilty
+delete_service = services.delete_service
+
+#locations
+get_loc_list = locations.get_loc_list
+get_loc_id = locations.get_loc_id
+get_loc_name = locations.get_loc_name
+
+#bookings
+create_booking = bookings.create_booking
+
