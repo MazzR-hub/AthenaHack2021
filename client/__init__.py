e3c511514aa543cbfb3ea331cfe813693342a449
Lauren Cooper <sc18lrec@leeds.ac.uk>
2021-04-17 13:24:07 +0100

    blueprint connected

--- client/__init__.py
+++ client/__init__.py
@@ -1,7 +1,17 @@
 from flask import Flask
 
+from client.views.homepage import homepage
+from client.views.login import login
+from client.views.userpage import userpage
+from client.views.checkout import checkout
+from client.views.listings import listings
+
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+
+app.register_blueprint(homepage)
+app.register_blueprint(login, url_prefix='/login')
+app.register_blueprint(userpage, url_prefix='/userpage')
+app.register_blueprint(checkout, url_prefix='/checkout')
+app.register_blueprint(listings, url_prefix='/listings')
--- client/views/homepage.py
+++ client/views/homepage.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-frontpage = Blueprint('homepage', __name__, template_folder='templates',
+homepage = Blueprint('homepage', __name__, template_folder='templates',
                       static_folder='static')
 
 homepage_url = 'index.html'
