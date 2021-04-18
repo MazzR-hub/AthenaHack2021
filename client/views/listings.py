8cf2f4cb97d7652fae288539bfa4bcc40bea4c8a
Lauren Cooper <sc18lrec@leeds.ac.uk>
2021-04-18 12:42:50 +0100

    login not working

--- client/views/listings.py
+++ client/views/listings.py
@@ -6,7 +6,20 @@ listings = Blueprint('listings', __name__, template_folder='templates', static_f
 
 @listings.route('/listings', methods=['GET', 'POST'])
 def display_listings():
+    new_listing = {'name': 'Lawn Mower',
+     'available': True,
+     'location': 'London',
+     'serviceType': 'Gardening Tool',
+     'points': 100,
+     'price' :20,
+     'user_id': 123
+     }
+    api.add_services(new_listing)
     listedItems = api.get_servicelist()
     if listedItems is None:
         listedItems = ['No listings available in your area! Why don\'t you add one?']
     return render_template('listings.html', listedItems=listedItems)
+
+@listings.route('/newlist', methods=['GET', 'POST'])
+def display_newlist():
+    return render_template('newlist.html')
--- client/views/userpage.py
+++ client/views/userpage.py
@@ -1,10 +1,18 @@
 from flask import Blueprint, render_template
+from client.database import api
 
 userpage = Blueprint('userpage', __name__, template_folder='templates',
                  static_folder='static')
 
-userpage_url = 'userpage.html'
+userpage_url = 'account.html'
 
-@userpage.route('/'+ userpage_url,  methods=['GET', 'POST'])
+@userpage.route('/account',  methods=['GET', 'POST'])
 def display_userpage():
-    return render_template(userpage_url)
+    myBookings = api.get_user_details(000)
+    if myBookings is None:
+        myBookings = ["You have no bookings"]
+    return render_template(userpage_url, myBookings=myBookings)
+
+@userpage.route('/details', methods=['GET', 'POST'])
+def display_details():
+    return render_template('details.html')
